c4fc032e397c2cb4dc6d80da7c54ffbdba9a20fd
tmdtjq211-tech <tmdtjq211@gmail.com>
2026-01-12 21:39:31 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import PlaneGame from "../components/plane-game"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <PlaneGame />
+    </main>
+  )
+}
