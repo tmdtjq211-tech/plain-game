bda469876b7ee95e2a7c89580b719da6f897ae98
tmdtjq211-tech <tmdtjq211@gmail.com>
2026-05-15 13:15:13 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PlaneGame from "../components/plane-game"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black">
-      <PlaneGame />
-    </main>
-  )
-}
